92c5bd774fa3367c261316e03e751008215bf7d1
RabotaemActivno <fajzovigor5@gmail.com>
2023-07-07 15:28:26 +0300

    finished 1

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,9 +1,39 @@
+import { Metadata } from "next"
+import Link from "next/link"
+
 type Props = {
     params: {
-        id:string
+        id: string
     }
 }
 
-export default function Post({params: {id}}: Props) {
-    return <h1>Post Page {id}</h1>
+async function getData(id: string) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        next: {
+            revalidate: 60
+        }
+    });
+    return response.json()
+}
+export async function generateMetadata({
+    params: { id },
+}: Props): Promise<Metadata> {
+    const post = await getData(id)
+
+    return {
+        title: post.title
+    }
+}
+
+export default async function Post({ params: { id } }: Props) {
+
+    const post = await getData(id)
+
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <Link href='/blog'>Назад</Link>
+        </div>
+    )
 }
