bedc949c92868bff555a960e3160bce9f8e6427d
RabotaemActivno <fajzovigor5@gmail.com>
2023-07-10 19:38:40 +0300

    swr

---
+++ component/Posts.tsx
@@ -0,0 +1,30 @@
+'use client'
+import shallow from 'zustand/shallow'
+import { usePosts } from '@/store'
+import { useEffect } from 'react'
+import Link from "next/link"
+
+const Posts = () => {
+
+    const [posts, loading, getAllPosts] = usePosts(state => [
+        state.posts, 
+        state.loading,
+        state.getAllPosts
+    ], shallow)
+
+    useEffect(()=> {
+        getAllPosts()
+    }, [getAllPosts])
+
+    return ( loading ? (<h3>Loading ...</h3>) : (
+        <ul>
+            {posts.map((post: any) => (
+                <li key={post.id}>
+                    <Link href={`/blog/${post.id}`}>{post.title}</Link>
+                </li>
+            ))}
+        </ul>
+    ))
+}
+
+export {Posts}
---
+++ component/PostsSearch.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { FormEventHandler, useState } from "react"
+import { usePosts } from "@/store"
+
+
+
+const PostSearch = () => {
+    const [search, setSearch] = useState('')
+    const getPostsBySearch = usePosts(state => state.getPostsBySearch)
+
+const handleSubmit: FormEventHandler<HTMLFormElement> =async (e) => {
+    e.preventDefault()
+
+    await getPostsBySearch(search)
+}
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="search" 
+                placeholder="search" 
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)}/>
+            <button type="submit">Search</button>
+        </form>
+    )
+}
+
+export {PostSearch}
